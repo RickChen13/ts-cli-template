9f29f2d78ce679dee362cbe6dc6bd68099f50193
RickChen13 <rickchen13@gmail.com>
2023-07-17 12:57:40 +0800

    添加深拷贝函数

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
 import Calendar from "@/common/Calendar";
+import { deepCopy } from "./common/common";
 
-const calendar = new Calendar();
+//#region 日历信息转换
+function calendar_test() {
+    const calendar = new Calendar();
+    let str = calendar.lunar2solar(2023, 2, 1, true);
+    console.log("str", str);
+}
+//#endregion
 
-let str = calendar.lunar2solar(2023, 2, 1, true);
+//#region 深拷贝
+function deepCopy_test() {
+    let obj = {
+        name: "rick",
+        age: 18,
+        list: [1, 2, 3, "x"],
+        func: function () {
+            console.log(`hello my name is ${this.name}`);
+        }
+    }
+    let obj1 = deepCopy(obj);
+    let obj2 = obj;
+    obj.list[0] = 0;
 
-console.log("str", str);
+    console.log('obj', obj);
+    console.log('obj1', obj1);
+    console.log('obj2', obj2);
+}
+//#endregion
